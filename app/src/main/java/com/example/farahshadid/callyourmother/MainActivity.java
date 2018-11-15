1722e7a119d84d438fc8ecd7d20b941d750f512c
Farah Shadid <fshadid@terpmail.umd.edu>
2018-11-15 01:29:17 -0500

    Firebase interaction methods(incomplete)

--- app/src/main/java/com/example/farahshadid/callyourmother/MainActivity.java
+++ app/src/main/java/com/example/farahshadid/callyourmother/MainActivity.java
@@ -19,34 +19,11 @@ import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
-    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
-    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-//
-//        myRef.setValue("Hello, World!");
 
-
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
