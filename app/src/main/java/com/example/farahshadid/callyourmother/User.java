3fdf673085e4376121db6d784e63eff258d83938
Farah Shadid <fshadid@terpmail.umd.edu>
2018-11-16 02:38:30 -0500

    tests
    tested: writeNewUsers, removeUsers, updateCommitmentScore

--- app/src/main/java/com/example/farahshadid/callyourmother/User.java
+++ app/src/main/java/com/example/farahshadid/callyourmother/User.java
@@ -1,74 +1,55 @@
 package com.example.farahshadid.callyourmother;
-
-import android.nfc.Tag;
-import android.widget.Toast;
 import android.util.Log;
-
-import com.google.firebase.database.IgnoreExtraProperties;
-import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-
 import java.util.ArrayList;
 
 public class User {
     public String username;
     public ArrayList<String> topContacts;
     public int commitmentScore;
-
     private DatabaseReference mDatabase;
-    private static final String TAG = "Firebase Problem";
+    private FirebaseDatabase database;
+    private static final String TAG = "Firebase Activity: ";
 
-    //Default Constructor
-    public User(){
-
-    }
-
-    public User(String username,String[] topContacts, int commitmentScore){
+    public User(String username,ArrayList<String> topContacts, int commitmentScore){
         this.username = username;
-        this.topContacts = new ArrayList<String>();
+        this.topContacts = topContacts;
         this.commitmentScore = commitmentScore;
+        mDatabase = FirebaseDatabase.getInstance().getReference().getRoot();
+        database = FirebaseDatabase.getInstance();
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 
-    private void writeNewUser(String userId, String[] topContactsList) {
+    /**
+     * add a new user under users in the database with a commitment Score of 0
+     * @param userId
+     * @param topContactsList
+     */
+    public void writeNewUser(String userId, ArrayList<String> topContactsList) {
         User user = new User(userId,topContactsList,0);
-
-        mDatabase.child("users").child(userId).setValue(user);
+        mDatabase.child("Users").child(userId).setValue(user);
+        Log.v(TAG, "inserted " + userId + " to the database");
     }
 
-//    private void addTopContact(User user, String topContact) {
-//        if(user.topContacts.size() <= 5){
-//            mDatabase.child("users").child(user.username).child("top contacts").setValue(user.topContacts.add(topContact));
-//        }
-//        else{
-//            // Add an alert dialouge
-//           Log.e(TAG,"Please Remove a top contact");
-//        }
-//
-//        }
+    /**
+     * remove a user from the data base
+     * @param user
+     */
 
-    private void removeUser(User user){
-        mDatabase.child("users").child(user.username).removeValue();
-    }
-    
-    private void updateCommitmentScore(User user,int newCommitmentScore){
-        mDatabase.child("users").child(user.username).child("commitment score").setValue(newCommitmentScore);
+    public void removeUser(User user){
+        mDatabase.child("Users").child(user.username).removeValue();
+        Log.v(TAG, "removed " + user.username + " from the database");
     }
 
-
-
-//    private void removeTopContact(User user, String topContactToDelete){
-//        int index = user.topContacts.indexOf(topContactToDelete);
-//        user.topContacts.remove(index);
-//        if(index != -1){
-//            mDatabase.child("users").child("top contacts").removeValue(user.topContacts.remove(index));
-//        }
-//
-//    }
-
+    /**
+     * update the commitmentScore of a user
+     * @param user
+     * @param newCommitmentScore
+     */
+    public void updateCommitmentScore(User user,int newCommitmentScore){
+        mDatabase.child("Users").child(user.username).child("commitmentScore").setValue(newCommitmentScore);
+        Log.v(TAG, "Updated " + user.username + "'s from the database");
     }
+
+}
