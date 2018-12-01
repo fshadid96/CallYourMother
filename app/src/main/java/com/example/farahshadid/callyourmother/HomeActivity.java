6cffb4d431592b3b22298eb67fed717f7af2f20e
MarquelH <marquelhendricks96@gmail.com>
2018-12-01 18:39:02 -0500

    Took out back button

--- app/src/main/java/com/example/farahshadid/callyourmother/HomeActivity.java
+++ app/src/main/java/com/example/farahshadid/callyourmother/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.farahshadid.callyourmother;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
