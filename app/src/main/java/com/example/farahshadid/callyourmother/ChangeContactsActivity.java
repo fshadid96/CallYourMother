e56b1e9196db8b77dd50745ff59c3bbe0a1a0d69
Farah Shadid <farahshadid@gmail.com>
2018-12-02 01:32:57 -0500

    added change contact activity

--- app/src/main/java/com/example/farahshadid/callyourmother/ChangeContactsActivity.java
+++ app/src/main/java/com/example/farahshadid/callyourmother/ChangeContactsActivity.java
@@ -1,4 +1,23 @@
 package com.example.farahshadid.callyourmother;
 
-public class ChangeContactsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class ChangeContactsActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_change);
+
+    }
+
 }
+
