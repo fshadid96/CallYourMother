7df1f7dd811b4f45dddd3cda6b52fc3e21fd3538
Farah Shadid <farahshadid@gmail.com>
2018-12-02 01:31:13 -0500

    added the change contact activity

--- app/src/main/java/com/example/farahshadid/callyourmother/SettingsFragment.java
+++ app/src/main/java/com/example/farahshadid/callyourmother/SettingsFragment.java
@@ -7,12 +7,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import java.util.ArrayList;
 
 public class SettingsFragment extends Fragment {
-
+        Button button;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        button = (Button)view.findViewById(R.id.contacts_button);
+
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent changeContactIntent = new Intent(view.getContext(), ChangeContactsActivity.class);
+                startActivity(changeContactIntent);
+            }
+        });
+
+
+        return view;
     }
 }
