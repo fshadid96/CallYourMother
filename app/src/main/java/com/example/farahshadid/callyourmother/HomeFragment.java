647d5b820e004757ca0704e587a8cc76838fee2f
emancho <emancho@umd.edu>
2018-12-03 03:54:11 -0500

    Removed the suggestions and only focused on the top 5. Feature of number showing when text is pushed still in

--- app/src/main/java/com/example/farahshadid/callyourmother/HomeFragment.java
+++ app/src/main/java/com/example/farahshadid/callyourmother/HomeFragment.java
@@ -1,20 +1,31 @@
 package com.example.farahshadid.callyourmother;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
 
-    private Button topConButton1, topConButton2, topConButton3, topConButton4,
-            topConButton5, sugConButton1, sugConButton2, sugConButton3 ;
-    private int b1 = 0,b2 = 0,b3 = 0,b4 = 0,
-            b5 = 0,s1 = 0,s2 = 0,s3 = 0;
+    private DatabaseReference mDatabase;
+    private TextView topConButton1, topConButton2, topConButton3, topConButton4,
+            topConButton5, sugConButton1, sugConButton2, sugConButton3;
+    private String userName, uname1, uname2, uname3, uname4, uname5,
+            num1, num2, num3, num4, num5;
+    private String TAG = "HomeFragment";
+    private int b1 = 0, b2 = 0, b3 = 0, b4 = 0, b5 = 0;
 
     @Nullable
     @Override
@@ -22,130 +33,175 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
+        topConButton1 = v.findViewById(R.id.top_con1);
+        topConButton2 = v.findViewById(R.id.top_con2);
+        topConButton3 = v.findViewById(R.id.top_con3);
+        topConButton4 = v.findViewById(R.id.top_con4);
+        topConButton5 = v.findViewById(R.id.top_con5);
+
+        userName = getDeviceName();   // Placeholder
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         //=========================================== Top Contacts
+        mDatabase.child("Users").child(userName).child("topContacts").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int count = 0;
 
-        topConButton1 = v.findViewById(R.id.top_con1);
-        // This is will be where we'll call the get top con and set it to teh button, same applies
-        // to the other ones
-        topConButton1.setText("Hello");
-        topConButton1.setOnClickListener(this);
+                for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
 
-        topConButton2 = v.findViewById(R.id.top_con2);
-        topConButton2.setText("You");
-        topConButton2.setOnClickListener(this);
+                    // This gets the name of the on of the top contacts and will be used to get the
+                    // the number
+                    String personName = (String) childSnapshot.child("name").getValue();
+                    String personNumber = (String) childSnapshot.child("numbers").child("0").getValue();
 
-        topConButton3 = v.findViewById(R.id.top_con3);
-        topConButton3.setText("There");
-        topConButton3.setOnClickListener(this);
+                    Log.i(TAG, "The person is: " + personName);
+                    Log.i(TAG, "the number is: " + personNumber);
+                    switch (count) {
+                        case 0:
+                            uname1 = personName;
+                            num1 = personNumber;
 
-        topConButton4 = v.findViewById(R.id.top_con4);
-        topConButton4.setText("Uhhh");
-        topConButton4.setOnClickListener(this);
+                            topConButton1.setText(personName);
+                            topConButton1.setVisibility(View.VISIBLE);
+                            topConButton1.setOnClickListener(HomeFragment.this);
+                            break;
+                        case 1:
+                            uname2 = personName;
+                            num2 = personNumber;
 
-        topConButton5 = v.findViewById(R.id.top_con5);
-        topConButton5.setText("yeah");
-        topConButton5.setOnClickListener(this);
+                            topConButton2.setText(personName);
+                            topConButton2.setVisibility(View.VISIBLE);
+                            topConButton2.setOnClickListener(HomeFragment.this);
+                            break;
+                        case 2:
+                            uname3 = personName;
+                            num3 = personNumber;
 
+                            topConButton3.setText(personName);
+                            topConButton3.setVisibility(View.VISIBLE);
+                            topConButton3.setOnClickListener(HomeFragment.this);
+                            break;
+                        case 3:
+                            uname4 = personName;
+                            num4 = personNumber;
+
+                            topConButton4.setText(personName);
+                            topConButton4.setVisibility(View.VISIBLE);
+                            topConButton4.setOnClickListener(HomeFragment.this);
+                            break;
+                        case 4:
+                            uname5 = personName;
+                            num5 = personNumber;
+
+                            topConButton5.setText(personName);
+                            topConButton5.setVisibility(View.VISIBLE);
+                            topConButton5.setOnClickListener(HomeFragment.this);
+                            break;
+                    }
+                    count += 1;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.i(TAG, "Something kinda Bad Happened!");
+            }
+        });
+        // Suggestions
 
         //============================================ Sugggestions
-        sugConButton1 = v.findViewById(R.id.sugg_con1);
-        sugConButton1.setText("Thank you");
-        sugConButton1.setOnClickListener(this);
-
-        sugConButton2 = v.findViewById(R.id.sugg_con2);
-        sugConButton2.setText("Based");
-        sugConButton2.setOnClickListener(this);
-
-        sugConButton3 = v.findViewById(R.id.sugg_con3);
-        sugConButton3.setText("God");
-        sugConButton3.setOnClickListener(this);
-
+//        sugConButton1 = v.findViewById(R.id.sugg_con1);
+//        sugConButton1.setText("Thank you");
+//        sugConButton1.setOnClickListener(this);
+//
+//        sugConButton2 = v.findViewById(R.id.sugg_con2);
+//        sugConButton2.setText("Based");
+//        sugConButton2.setOnClickListener(this);
+//
+//        sugConButton3 = v.findViewById(R.id.sugg_con3);
+//        sugConButton3.setText("God");
+//        sugConButton3.setOnClickListener(this)
 
         return v;
     }
 
+    public String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return capitalize(model);
+        } else {
+            return capitalize(manufacturer) + " " + model;
+        }
+    }
+
+    private String capitalize(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        char first = s.charAt(0);
+        if (Character.isUpperCase(first)) {
+            return s;
+        } else {
+            return Character.toUpperCase(first) + s.substring(1);
+        }
+    }
+
+
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
 
+        Log.i(TAG, "method is doing stuff supposely");
+
+        switch (v.getId()) {
             case R.id.top_con1:
-                if(b1 == 0){
-                    topConButton1.setText("22222222");
+                if (b1 == 0) {
+                    topConButton1.setText(num1);
                     b1 = 1;
-                }else{
-                    topConButton1.setText("Hello");
+                } else {
+                    topConButton1.setText(uname1);
                     b1 = 0;
                 }
                 break;
 
             case R.id.top_con2:
-                if(b2 == 0){
-                    topConButton2.setText("111111111");
+                if (b2 == 0) {
+                    topConButton2.setText(num2);
                     b2 = 1;
-                }else{
-                    topConButton2.setText("You");
+                } else {
+                    topConButton2.setText(uname2);
                     b2 = 0;
                 }
                 break;
 
             case R.id.top_con3:
-                if(b3 == 0){
-                    topConButton3.setText("3232323232");
+                if (b3 == 0) {
+                    topConButton3.setText(num3);
                     b3 = 1;
-                }else{
-                    topConButton3.setText("There");
+                } else {
+                    topConButton3.setText(uname3);
                     b3 = 0;
                 }
                 break;
 
             case R.id.top_con4:
-                if(b4 == 0){
-                    topConButton4.setText("535355353");
+                if (b4 == 0) {
+                    topConButton4.setText(num4);
                     b4 = 1;
-                }else{
-                    topConButton4.setText("Uhhh");
+                } else {
+                    topConButton4.setText(uname4);
                     b4 = 0;
                 }
                 break;
 
             case R.id.top_con5:
-                if(b5 == 0){
-                    topConButton5.setText("646646464646");
+                if (b5 == 0) {
+                    topConButton5.setText(num5);
                     b5 = 1;
-                }else{
-                    topConButton5.setText("yeah");
+                } else {
+                    topConButton5.setText(uname5);
                     b5 = 0;
-                }
-                break;
-//==========================================================
-            case R.id.sugg_con1:
-                if(s1 == 0){
-                    sugConButton1.setText("77757577575");
-                    s1 = 1;
-                }else{
-                    sugConButton1.setText("Thank you");
-                    s1 = 0;
-                }
-                break;
-
-            case R.id.sugg_con2:
-                if(s2 == 0){
-                    sugConButton2.setText("683839857");
-                    s2 = 1;
-                }else{
-                    sugConButton2.setText("Based");
-                    s2 = 0;
-                }
-                break;
-
-            case R.id.sugg_con3:
-                if(s3 == 0){
-                    sugConButton3.setText("1113445533");
-                    s3 = 1;
-                }else{
-                    sugConButton3.setText("God");
-                    s3 = 0;
                 }
                 break;
         }
